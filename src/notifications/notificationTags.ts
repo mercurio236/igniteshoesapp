b96d00b3e376d2c10afd44543a1ec1181fedbebb
Arley Souto <macx15@hotmail.com>
2023-07-17 18:08:16 -0300

    push notification do carrinho abandonado

--- src/notifications/notificationTags.ts
+++ src/notifications/notificationTags.ts
@@ -3,3 +3,7 @@ import OneSignal from "react-native-onesignal";
 export function TagUserEmailCreate(email:string){
     OneSignal.sendTag('user_email', email)
 }
+
+export function tagCartUpdate(itemsCount:string){
+    OneSignal.sendTag('cart_item_count', itemsCount)
+}
